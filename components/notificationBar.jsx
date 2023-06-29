384878763515f04fcbc825a92c099c86020b08b1
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-06-29 17:28:21 +0300

    test

--- components/notificationBar.jsx
+++ components/notificationBar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { IoNotificationsOutline, IoCaretBackCircleOutline } from 'react-icons/io5'
 const NotificationBar = () => {
     return (
-        <div className='w-full bg-white rounded-tl-full '>
-            <div className='w-full flex items-center justify-between text-gray-300 text-2xl md:container mx-auto p-4 md:px-12 xl:px-16 border-b-[1px] border-gray-100 shadow-sm'>
+        <div className='w-full bg-white rounded-tl-full shadow-sm bg-red-500'>
+            <div className='w-full flex items-center justify-between text-gray-300 text-2xl md:container mx-auto p-4 md:px-12 xl:px-16 border-b-[1px] border-gray-100 '>
                 <IoCaretBackCircleOutline />
                 <IoNotificationsOutline />
             </div>
