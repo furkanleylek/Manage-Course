1a3f99613f5f615e504fe5e0c2678b6a3ac62ad5
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-06-30 21:54:51 +0300

    notification-bar

--- components/notification-bar.jsx
+++ components/notification-bar.jsx
@@ -3,7 +3,7 @@ import { IoNotificationsOutline, IoCaretBackCircleOutline } from 'react-icons/io
 import ThemeToggle from './theme-toggle'
 const NotificationBar = () => {
     return (
-        <div className='w-full bg-bar rounded-tl-full shadow-sm shadow-border'>
+        <div className='w-full bg-bar md:rounded-tl-full shadow-sm shadow-border'>
             <div className='w-full flex items-center justify-between text-gray-300 text-2xl md:container mx-auto p-4 md:px-12 xl:px-16  '>
                 <IoCaretBackCircleOutline />
                 <ThemeToggle />
